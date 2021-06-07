0c99cd5adaec3f7d634010a87fbe12b94cf2bd83
i582 <i58215@voenmeh.ru>
2021-06-07 07:14:01 +0300

    added rule checker

--- README.md
+++ README.md
@@ -7,18 +7,23 @@ See [nocolor](https://github.com/i582/phpcolor).
 
 - Quick jump to the rule in the palette;
 
-  <img width="295" alt="phpdoc types" src="https://user-images.githubusercontent.com/51853996/120945069-59998600-c740-11eb-94fb-fd88d4027f92.png">
+  <img width="295" alt="image" src="https://user-images.githubusercontent.com/51853996/120945069-59998600-c740-11eb-94fb-fd88d4027f92.png">
 
-- Autocomplete color names.
+- Autocomplete color names;
 
-  <img width="350" alt="phpdoc types" src="https://user-images.githubusercontent.com/51853996/120945146-a2513f00-c740-11eb-92e1-6026706a9303.png">
+  <img width="350" alt="image" src="https://user-images.githubusercontent.com/51853996/120945146-a2513f00-c740-11eb-92e1-6026706a9303.png">
   
 - Color checks: duplicate colors, undefined colors, etc.;
 
-  <img width="367" alt="phpdoc types" src="https://user-images.githubusercontent.com/51853996/120945215-db89af00-c740-11eb-972c-871e46cabf22.png">
+  <img width="367" alt="image" src="https://user-images.githubusercontent.com/51853996/120945215-db89af00-c740-11eb-972c-871e46cabf22.png">
 
-  <img width="500" alt="phpdoc types" src="https://user-images.githubusercontent.com/51853996/120952603-12b58b80-c754-11eb-9588-f675c6446ea1.png">
+  <img width="500" alt="image" src="https://user-images.githubusercontent.com/51853996/120952603-12b58b80-c754-11eb-9588-f675c6446ea1.png">
+
+- Rules checks: order, structure, etc.
+
+  <img width="600" alt="image" src="https://user-images.githubusercontent.com/51853996/120958102-a6408980-c75f-11eb-9b00-0d6d6f4ac722.png">
 
 ## License
 
 This project is under the **MIT License**. See the [LICENSE](	) file for the full license text.
+
---
+++ src/main/kotlin/com/vk/nocolor/inspections/InvalidRuleOrder.kt
@@ -0,0 +1,50 @@
+package com.vk.nocolor.inspections
+
+import com.intellij.codeInspection.LocalInspectionTool
+import com.intellij.codeInspection.ProblemsHolder
+import com.intellij.psi.PsiElementVisitor
+import org.jetbrains.yaml.psi.YAMLKeyValue
+import org.jetbrains.yaml.psi.YAMLQuotedText
+import org.jetbrains.yaml.psi.YAMLSequence
+import org.jetbrains.yaml.psi.YamlPsiElementVisitor
+
+class InvalidRuleOrder : LocalInspectionTool() {
+    override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor {
+        return object : YamlPsiElementVisitor() {
+            override fun visitSequence(sequence: YAMLSequence) {
+                if (!sequence.containingFile.name.endsWith("palette.yaml")) {
+                    return
+                }
+
+                var errorRuleFound = false
+
+                for (item in sequence.items) {
+                    val keys = item.keysValues
+                    if (keys.size > 1) {
+                        keys.forEachIndexed { index: Int, key: YAMLKeyValue ->
+                            if (index != 0) {
+                                holder.registerProblem(key, "Each element in group must have only one key-value pair. Place this pair in a separate element of the array (add '- ' before the pair)")
+                            }
+                        }
+                    }
+                    if (keys.isEmpty()) {
+                        continue
+                    }
+
+                    val mainKey = keys.toTypedArray()[0]
+                    if (mainKey.value == null) {
+                        continue
+                    }
+
+                    if (mainKey.value!! is YAMLQuotedText && mainKey.value!!.text.length == 2) {
+                        if (!errorRuleFound) {
+                            holder.registerProblem(mainKey, "In the group, first there should be rules describing the error, and then the exclusion rules")
+                        }
+                    } else {
+                        errorRuleFound = true
+                    }
+                }
+            }
+        }
+    }
+}
